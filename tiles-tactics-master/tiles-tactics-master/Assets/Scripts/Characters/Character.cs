9b8411c9279df8323572f7b3516cde20e4c5a288
Manuela Recio <manu138@hotmail.es>
2017-03-26 23:27:05 -0500

    no message

---
+++ tiles-tactics-master/tiles-tactics-master/Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Map;
+using System;
+
+namespace Characters
+{
+    [RequireComponent (typeof (TileMovement))]
+    public class Character: MonoBehaviour
+    {
+        private TileMovement movement;
+        public PathFind.Grid grid;
+
+        public void Start()
+        {
+
+         
+        }
+
+        private void Awake ()
+        {
+            movement = GetComponent<TileMovement> ();
+        }
+        private Vector2 MakeVector(PathFind.Point p)
+        {
+            return new Vector2(p.x, p.y);
+        }
+        public void MoveTo (Vector2 tile)
+        {
+            //TODO: pathfinding
+
+            Tilemap.grid = this.grid;
+            PathFind.Point _from = new PathFind.Point(1, 1);
+            PathFind.Point _to = new PathFind.Point(7, 7);
+            List<PathFind.Point> path = PathFind.Pathfinding.FindPath(grid, _from, _to);
+            List<Vector2> lp = path.ConvertAll<Vector2>(MakeVector);
+          
+
+
+
+            movement.Move (lp);
+        }
+
+        private static List<Vector2> GetDummyPath ()
+        {
+          
+            List<Vector2> path = new List<Vector2> ();
+            path.Add (new Vector2(0, 0));
+            path.Add (new Vector2(0, 1));
+            path.Add (new Vector2(1, 1));
+            path.Add (new Vector2(2, 1));
+            path.Add (new Vector2(2, 2));
+            path.Add (new Vector2(2, 3));
+            return path;
+        }
+    }
+}
---
+++ tiles-tactics-master/tiles-tactics-master/Assets/Scripts/Map/Tilemap.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+namespace Map
+{
+	public class Tilemap : MonoBehaviour 
+	{
+		public TileRow[] tiles;
+        public float tileSize = 1f;
+        public int height = 8;
+        public int width = 8;
+        public float[,] tilesmap;
+        public static PathFind.Grid grid;
+
+        public void Start()
+        {
+            tilesmap = new float[width, height];
+           
+            for (int i = 1; i < width; i++)
+            {
+                for (int j = 1; j < height; j++)
+                {
+                    tilesmap[i, j] = 1;
+                }
+            }
+            for (int i = 0; i < width; i++)
+            {
+                
+                    tilesmap[0, i] = 0;
+                
+            }
+            for (int i = 0; i < height; i++)
+            {
+
+                tilesmap[i, 0] = 0;
+
+            }
+            for (int i = 0; i < height; i++)
+            {
+
+                tilesmap[i, 7] = 0;
+
+            }
+
+            grid = new PathFind.Grid(width, height, tilesmap);
+        }
+
+        public Vector2 TileToWorldPosition (Vector2 position)
+		{
+			return position * tileSize;
+		}
+
+		public Vector2 WorldToTilePosition (Vector2 position)
+		{
+			return position / tileSize;
+		}
+
+		public Tile GetTileAt (Vector2 position)
+		{
+			return tiles[(int)position.y].tiles[(int)position.x];
+		}
+	}
+}
