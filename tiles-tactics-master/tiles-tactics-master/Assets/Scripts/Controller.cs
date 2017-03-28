399ffca8ff087d70628e7cc2180f3f0f55058de4
manu138 <manu138@hotmail.es>
2017-03-28 13:52:59 -0500

    Tomouse

--- tiles-tactics-master/tiles-tactics-master/Assets/Scripts/Controller.cs
+++ tiles-tactics-master/tiles-tactics-master/Assets/Scripts/Controller.cs
@@ -7,13 +7,22 @@ public class Controller: MonoBehaviour
     private Character character;
     [SerializeField]
     private Vector2 tilePosition;
-
+    private Vector3 mousePos;
+    public void start()
+    {
+       
+    }
     private void Update ()
 
     {
-
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            tilePosition = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
             character.MoveTo(tilePosition);
+        }
+           
            
     }
 }
